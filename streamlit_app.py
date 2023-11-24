1e7cc78215ad4495ce11eadb9522941c62494dc9
VictorLECHOVOLEAbe <145446206+VictorLECHOVOLEAbe@users.noreply.github.com>
2023-11-24 14:19:00 +0200

    Update streamlit_app.py
    imported pandas
    file_link added
    stored in a python variable the content read with pandas from csv
    displayed with streamlit the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+file_link = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pandas.read_csv(file_link)
+streamlit.dataframe(my_fruit_list)

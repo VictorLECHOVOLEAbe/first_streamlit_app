95f918a2a0d7f2bf2bcf1a8b7385c76bfb6238f3
VictorLECHOVOLEAbe <145446206+VictorLECHOVOLEAbe@users.noreply.github.com>
2023-11-27 22:15:07 +0200

    Update streamlit_app.py
     Let's Get All the Rows, Not Just One

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,15 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("the fruit load list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("the fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+
+
+
+
+
 
 # # # # # # # # # # # # # # # 
 # import requests
@@ -49,3 +55,4 @@ streamlit.text(my_data_row)
 # # streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # streamlit.text("Hello from Snowflake:")
+# my_data_row = my_cur.fetchone()
